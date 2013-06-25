0eb313f8160bfea532ebe3bc8fa97b1a794c9842
Mike Aldred <mike.aldred@directcommunications.com.au>
2013-06-24 22:39:06 -0800

    Added local repository for deployment of snapshots.

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   :min-lein-version "2.0.0"
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
+  :repositories [["snapshots" "file://C:/Users/Mike Aldred/.m2/repository"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.luminousmonkey/javajom "6.3.9.1"]
                  [org.clojars.luminousmonkey/javajom-native "6.3.9.1"]]
